0425a3f208e8b65a64c05584024f518b3b0910d4
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-02 23:48:49 +0530

    [Shubham]Added: Inform user if entered invalid mood

--- src/test/java/com/capgi/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/capgi/moodanalyser/MoodAnalyserTest.java
@@ -7,10 +7,15 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 
 	@Test
-	public void givenMessage_WhenNull_ShouldReturnHappy() {
+	public void givenMessage_WhenNull_ShouldThrowMoodAnalysisException_ofTypeNUll() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-		String mood = moodAnalyser.analyseMood();
-		assertEquals("HAPPY", mood);
+		try {
+			moodAnalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.type);
+
+		}
+
 	}
 
 }
