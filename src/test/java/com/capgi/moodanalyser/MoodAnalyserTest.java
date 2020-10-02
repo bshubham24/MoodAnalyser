5ea0bb224b630089967cba13f8248b604d829dd5
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-02 21:42:57 +0530

    [Shubham]Updated: Check for any mood

--- src/test/java/com/capgi/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/capgi/moodanalyser/MoodAnalyserTest.java
@@ -9,8 +9,8 @@ public class MoodAnalyserTest {
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood = moodAnalyser.analyseMood("I am in Sad Mood");
-		assertEquals("SAD", mood);
+		String mood = moodAnalyser.analyseMood("I am in any Mood");
+		assertEquals("HAPPY", mood);
 	}
 
 }
