bca5f87cb28feb85d1c976218e27bac9335173c7
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-02 21:04:07 +0530

    [Shubham]Added: Test case for happy and sad mood

--- src/test/java/com/capgi/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/capgi/moodanalyser/MoodAnalyserTest.java
@@ -1,5 +1,23 @@
 package com.capgi.moodanalyser;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class MoodAnalyserTest {
+
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+		assertEquals("SAD", mood);
+	}
+
+	@Test
+	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("I am in Happy Mood");
+		assertEquals("HAPPY", mood);
+	}
 
 }
